a1681e6713bb02aafe7433eb90a1792f5ab2cbe9
“[MRAHXD]” <[sahebabdullah7860@gmail.com]>
2024-06-18 23:53:54 +0530

    added-signIn

---
+++ login.js
@@ -0,0 +1,64 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyABS5ki-PC-S1L31hj41YegoXWVe2Kq6c4",
+  authDomain: "qrgen-login-setup.firebaseapp.com",
+  projectId: "qrgen-login-setup",
+  storageBucket: "qrgen-login-setup.appspot.com",
+  messagingSenderId: "554423972068",
+  appId: "1:554423972068:web:331a69e9908afc44cdfcf1",
+  measurementId: "G-BZ9W4DYNRM",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//submit
+const submit = document.getElementById("login-btn");
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  // inputs
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      alert("log in...");
+      const isLogIn = true;
+
+      window.location.href="index.html"
+      
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage + "\nEnter correct email and password");
+      // ..
+    });
+});
+
+
+
+
+

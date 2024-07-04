b62022ea96c932b8ac6ddd124e402465c8bf5060
“[MRAHXD]” <[sahebabdullah7860@gmail.com]>
2024-07-05 00:32:12 +0530

    new-design

--- home.js
+++ home.js
@@ -1,8 +1,6 @@
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getDatabase , ref, set } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyAS0CCS0GWO3z_FcltqL-qpx6byiRVtoOY",
@@ -27,6 +25,83 @@ onAuthStateChanged(auth, (user) => {
     console.log("User is not signed in");
   }
 });
+
+const container = document.querySelector(".container");
+const userInput = document.getElementById("userInput");
+const submitBtn = document.getElementById("submit");
+const downloadBtn = document.getElementById("download");
+const sizeOptions = document.querySelector(".sizeOptions");
+const BGColor = document.getElementById("BGColor");
+const FGColor = document.getElementById("FGColor");
+let QR_Code;
+let sizeChoice, BGColorChoice, FGColorChoice, qrCodeDataURL;
+
+//Set size
+sizeOptions.addEventListener("change", () => {
+  sizeChoice = sizeOptions.value;
+});
+
+//Set background color
+BGColor.addEventListener("input", () => {
+  BGColorChoice = BGColor.value;
+});
+
+//Set foreground color
+FGColor.addEventListener("input", () => {
+  FGColorChoice = FGColor.value;
+});
+
+//Format input
+const inputFormatter = (value) => {
+  value = value.replace(/[^a-z0-9A-Z]+/g, "");
+  return value;
+};
+
+submitBtn.addEventListener("click", async () => {
+  container.innerHTML = "";
+  //QR code generation
+  QR_Code = await new QRCode(container, {
+    text: userInput.value,
+    width: sizeChoice,
+    height: sizeChoice,
+    colorDark: FGColorChoice,
+    colorLight: BGColorChoice,
+  });
+
+  // Store the data URL for later use
+  qrCodeDataURL = container.firstChild.toDataURL("image/png");
+  downloadBtn.classList.remove("hide");
+
+});
+
+downloadBtn.addEventListener("click", () => {
+  const downloadLink = document.createElement("a");
+  downloadLink.href = qrCodeDataURL;
+  downloadLink.download = `${inputFormatter(userInput.value)}QR.png`;
+  downloadLink.click();
+});
+
+userInput.addEventListener("input", () => {
+  if (userInput.value.trim().length < 1) {
+    submitBtn.disabled = true;
+    downloadBtn.href = "";
+    downloadBtn.classList.add("hide");
+  } else {
+    submitBtn.disabled = false;
+  }
+});
+
+window.onload = () => {
+  container.innerHTML = "";
+  sizeChoice = 100;
+  sizeOptions.value = 100;
+  userInput.value = "";
+  BGColor.value = BGColorChoice = "#ffffff";
+  FGColor.value = FGColorChoice = "#377dff";
+  downloadBtn.classList.add("hide");
+  submitBtn.disabled = true;
+};
+
 
  
 
